317d5c1204cecc33dde426d93bfdd7675b4c9e82
yaso <ym1647445@gmail.com>
2026-05-18 20:17:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
-const SUPABASE_URL = "https://bwtchfidnvlwyikhawua.supabase.co";
-
-const SUPABASE_KEY = "sb_publishable_u-jJWO8AsVfgxbSTXk_l8A_ldRMy4o2";
+const SUPABASE_URL="https://bwtchfidnvlwyikhawua.supabase.co";
+const SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3dGNoZmlkbnZsd3lpa2hhd3VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NDY3MzYsImV4cCI6MjA5NDQyMjczNn0.K4EX4yfqmAT3C7aR02Zj4f4n1CMY4va9v6rvz73LVIo";
+const BOT_TOKEN = "8734260364:AAF6CyGRTiGbLCr0oLbJmFF9J1QsIAssquw";
+const CHAT_ID = "8231091019";
+const ADMIN_PASSWORD="X3%k5yg-@siE/3J";
+const WHATSAPP_CHANNEL="https://whatsapp.com/channel/0029Vb7xg1EEwEk54XvWsl09";
+const FACEBOOK_URL="https://www.facebook.com/share/1CuTSXXPdY/";
+const CASH_NUMBER="010 359 66569";
