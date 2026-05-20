6a2e46485b316ba7ed9db1cd1c91f1907b33d48b
yaso <ym1647445@gmail.com>
2026-05-20 16:41:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 const SUPABASE_URL = "https://bwtchfidnvlwyikhawua.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3dGNoZmlkbnZsd3lpa2hhd3VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NDY3MzYsImV4cCI6MjA5NDQyMjczNn0.K4EX4yfqmAT3C7aR02Zj4f4n1CMY4va9v6rvz73LVIo";
 
-const ADMIN_PASSWORD = "X3%k5yg-@siE/3J";
-
 const BOT_TOKEN = "8734260364:AAFGJyGzTUD31u5fpvxcWwXxe1p05h4_LDw";
 const CHAT_ID = "8231091019";
 
