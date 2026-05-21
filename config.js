1d4a28fe1882e5ccaf5be330cdaeb27949ccad27
yaso <ym1647445@gmail.com>
2026-05-21 23:23:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,4 @@
-const SUPABASE_URL = "https://bwtchfidnvlwyikhawua.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3dGNoZmlkbnZsd3lpa2hhd3VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NDY3MzYsImV4cCI6MjA5NDQyMjczNn0.K4EX4yfqmAT3C7aR02Zj4f4n1CMY4va9v6rvz73LVIo";
-
-const BOT_TOKEN = "8734260364:AAFGJyGzTUD31u5fpvxcWwXxe1p05h4_LDw";
-const CHAT_ID = "8231091019";
-
-const WHATSAPP_CHANNEL = "https://whatsapp.com/channel/0029Vb7xg1EEwEk54XvWsl09";
-const FACEBOOK_URL = "https://www.facebook.com/share/1CuTSXXPdY/";
-const CASH_NUMBER = "010 359 66569";
-const SUPPORT_WHATSAPP = "201036797528";
-const WALLET_BOT_TOKEN = "8965754474:AAEPU73b3pz8f-5at3pjzVsCS5iyoxnc3go";
-const WALLET_CHAT_ID = "8231091019";
+const BOT_TOKEN = "";
+const CHAT_ID = "";
+const WALLET_BOT_TOKEN = "";
+const WALLET_CHAT_ID = "";
