6232fd978ecf8c49961f96289b9e99f9cc365cba
yaso <ym1647445@gmail.com>
2026-05-21 23:59:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const SUPABASE_URL = "https://bwtchfidnvlwyikhawua.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3dGNoZmlkbnZsd3lpa2hhd3VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NDY3MzYsImV4cCI6MjA5NDQyMjczNn0.K4EX4yfqmAT3C7aR02Zj4f4n1CMY4va9v6rvz73LVIo";
 
-const client = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3dGNoZmlkbnZsd3lpa2hhd3VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NDY3MzYsImV4cCI6MjA5NDQyMjczNn0.K4EX4yfqmAT3C7aR02Zj4f4n1CMY4va9v6rvz73LVIo";
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
