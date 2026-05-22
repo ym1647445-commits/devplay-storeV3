264b32a0be172013a7c7d934dd383c446a947461
yaso <ym1647445@gmail.com>
2026-05-22 13:36:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,4 @@ const SUPABASE_URL = "https://bwtchfidnvlwyikhawua.supabase.co";
 
 // حطي هنا anon public key من Supabase فقط
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3dGNoZmlkbnZsd3lpa2hhd3VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NDY3MzYsImV4cCI6MjA5NDQyMjczNn0.K4EX4yfqmAT3C7aR02Zj4f4n1CMY4va9v6rvz73LVIo";
+const WHATSAPP_CHANNEL = "https://wa.me/201036797528";
