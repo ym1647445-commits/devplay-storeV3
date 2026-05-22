5960ae78ac03253db017b52e7f8789e38e1115d4
yaso <ym1647445@gmail.com>
2026-05-22 13:17:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 const SUPABASE_URL = "https://bwtchfidnvlwyikhawua.supabase.co";
 
+// حطي هنا anon public key من Supabase فقط
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3dGNoZmlkbnZsd3lpa2hhd3VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NDY3MzYsImV4cCI6MjA5NDQyMjczNn0.K4EX4yfqmAT3C7aR02Zj4f4n1CMY4va9v6rvz73LVIo";
-
-const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
